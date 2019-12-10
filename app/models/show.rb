9c27330b5483213296a4e1fca667ed1634802631
Jazmin M <jazminmerckel@gmail.com>
2019-12-10 06:09:33 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,31 @@
 class Show < ActiveRecord::Base
 
+  def self.highest_rating
+    maximum(:rating)
+  end
+
+  def self.most_popular_show
+
+  end
+
+  def self.lowest_rating
+    minimum(:rating)
+  end
+
+  def self.least_popular_show
+
+  end
+
+  def self.ratings_sum
+    sum(:rating)
+  end
+
+  def self.popular_shows
+
+  end
+
+  def self.shows_by_alphabetical_order
+
+  end
+
 end
